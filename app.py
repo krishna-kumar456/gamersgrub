4e762e4b8c9b579ece3c569d92d6bed25df2c799
Krishna <krishnaqmar@gmail.com>
2017-05-22 06:10:12 +0530

    Fixed issues with map

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-GoogleMaps(app)
+GoogleMaps(app, key="AIzaSyC-guaMVPHp2e3QMOq_HLS7pZ1v12hPZRM")
 
 
 from models import Result
