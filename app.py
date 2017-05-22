23e1e2fcd81f01af50492b0c9c242db54cee5bb5
Krishna <krishnaqmar@gmail.com>
2017-05-22 05:36:36 +0530

    Front end addition

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
 import os
-
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+GoogleMaps(app)
+
 
 from models import Result
 
 
+# Set "homepage" to index.html
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+	mymap = Map(
+        identifier="no-controls-map",
+        zoom_control=False,
+        maptype_control=False,
+        scale_control=False,
+        streetview_control=False,
+        rotate_control=False,
+        fullscreen_control=False,
+        lat=28.6315,
+        lng=77.2167,
+        style="height:500px;width:100%;",
+        markers=[(28.6315, 77.2167)]
+    )
+	return render_template('index.html', mymap=mymap)
 
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
 
 
 if __name__ == '__main__':
